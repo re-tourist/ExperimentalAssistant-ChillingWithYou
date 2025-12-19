3112567cc1f0c6cb8214729f49986cfb4556e482
re-tourist <157592208+re-tourist@users.noreply.github.com>
2025-12-19 17:21:39 +0800

    Revert "Feat/run crud metrics tags"

--- backend/src/main/java/com/experimentalassistant/backend/mapper/MetricDefMapper.java
+++
@@ -1,9 +0,0 @@
-package com.experimentalassistant.backend.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.experimentalassistant.backend.entity.MetricDef;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface MetricDefMapper extends BaseMapper<MetricDef> {
-}
--- backend/src/main/java/com/experimentalassistant/backend/service/MetricDefService.java
+++
@@ -1,7 +0,0 @@
-package com.experimentalassistant.backend.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.experimentalassistant.backend.entity.MetricDef;
-
-public interface MetricDefService extends IService<MetricDef> {
-}
